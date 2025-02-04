3e46c85716a26454e82b190239ba2f88e14cc2e2
siddheswar-tripurari <siddheswar.tripurari@vonage.com>
2025-02-04 16:37:45 +0530

    Version 2

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 console.log("This is a config file.")
-console.log("Version 1")
+console.log("Version 2")
--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 console.log("This is a Index file")
-console.log("version 1");
+console.log("version 2");
